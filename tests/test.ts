fc55a5fe06dabaf30819e780a49607f5d2aeda78
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-09-20 16:18:24 +0200

    add test

--- tests/test.ts
+++ tests/test.ts
@@ -8,4 +8,18 @@ test('form will redirect and display card', async ({ page }) => {
 	expect(page.url().endsWith('/TorstenDittmann')).toBeTruthy();
 	await page.waitForLoadState('networkidle');
 	expect(await page.locator('#maker').count()).toEqual(1);
+	expect(await page.locator('input').count()).toEqual(0);
+});
+
+test('direct link', async ({ page }) => {
+	await page.goto('/TorstenDittmann', {
+		waitUntil: 'networkidle'
+	});
+	expect(await page.locator('#maker').count()).toEqual(1);
+	expect(await page.locator('input').count()).toEqual(1);
+});
+
+test('unknown username', async ({ page }) => {
+	await page.goto('/linux');
+	await page.waitForSelector("text=User 'linux' not found");
 });
